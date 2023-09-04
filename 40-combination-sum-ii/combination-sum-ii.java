4a491791f32a49fb86d3b02118addd84fde04faf
sameer shrestha <sameer.shresthaa@gmail.com>
2023-09-04 18:16:39 +0530

    Time: 2 ms (99.77%) | Memory: 42.6 MB (44.84%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,35 +1,23 @@
-class Solution 
-{
-    public List<List<Integer>> combinationSum2(int[] cand, int target) 
-    {
-        Arrays.sort(cand);
-        List<List<Integer>> r = new ArrayList();
-        find(r, cand, target, 0, new ArrayList());
-        
-        return r;
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(candidates);
+        findCombinations(0,candidates,target,ans,new ArrayList());
+        return ans;
     }
-    
-    void find(List<List<Integer>> result, int[] cand, int target, int start, List<Integer> cur)
-    {
-        if (target == 0)
-        {
-            result.add(new ArrayList(cur));
-            return;
+    private void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if(target == 0) {
+            ans.add(new ArrayList<>(ds)); 
+            return; 
         }
         
-        for (int i = start; i < cand.length; ++i)
-        {
-            if (cand[i] <= target)
-            {
-                if (i == start || cand[i] != cand[i-1])
-                {
-                    int pos = cur.size();
-                    cur.add(cand[i]);
-                    find(result, cand, target - cand[i], i + 1, cur);
-                    cur.remove(pos);
-                }
-            }
-            else break;
+        for(int i = ind; i < arr.length;i++) {
+            if(i > ind && arr[i] == arr[i-1]) continue; 
+            if(arr[i]>target) break; 
+            
+            ds.add(arr[i]); 
+            findCombinations(i+1, arr, target - arr[i], ans, ds); 
+            ds.remove(ds.size() - 1); 
         }
     }
 }
